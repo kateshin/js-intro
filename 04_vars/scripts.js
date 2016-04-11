16430c398964e2558118d4e46c8dbb183914ad13
kateshin <kateshin@wustl.edu>
2016-04-11 18:55:52 -0500

    edited page 4

---
+++ 04_vars/scripts.js
@@ -0,0 +1,12 @@
+var heading = "";
+var content = "";
+
+heading = "this heading"
+
+content += "<h1>" + heading + "</h1>";
+
+content += "<p>the first text area. </p>";
+
+// document.write(content); (other option to console.log); console.log is a way to give feedback without showing on the page
+
+console.log(content);
